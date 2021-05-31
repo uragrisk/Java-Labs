8f43c5575dc7ca0f7ceff83da02290d4c61af140
uragrisk <george00958@gmail.com>
2021-05-31 11:21:42 +0300

    Adding Project Files

--- src/main/java/ua/lviv/iot/shipManager/controllers/ShipController.java
+++ src/main/java/ua/lviv/iot/shipManager/controllers/ShipController.java
@@ -1,14 +1,16 @@
 package ua.lviv.iot.shipManager.controllers;
 
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ua.lviv.iot.shipManager.exeptions.IdProvidedWhileCreationException;
+import ua.lviv.iot.shipManager.exeptions.IdProvidedWhileUpdatingException;
 import ua.lviv.iot.shipManager.models.Ship;
 import ua.lviv.iot.shipManager.services.ShipService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 
 @RestController
@@ -19,53 +21,59 @@ public class ShipController {
     private ShipService shipService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Ship> getShip(@PathVariable("id") Integer id) {
-        Ship existingShip = shipService.getShipById(id);
-        if (existingShip != null) {
-            ResponseEntity.ok(shipService.getShipById(id));
-            return ResponseEntity.ok(existingShip);
-        } else {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity getShipById(@PathVariable Integer id) {
+        try {
+            Ship existingSaw = shipService.getShipById(id);
+            return ResponseEntity.ok(existingSaw);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(404).body(
+                    "Can't get saw with id " + id + ". Such saw doesn't exist"
+            );
         }
     }
-//new Ship(100.0, 25, 23.5, SurfaceType.WATER, MovementType.PROPELLERS);
+
     @GetMapping
-    public ResponseEntity<List<Ship>> getShips(){
+    public ResponseEntity<List<Ship>> getShips() {
         return ResponseEntity.ok(shipService.getShips());
     }
 
     @PostMapping
-    public Ship createShip(@RequestBody Ship ship){
-        return shipService.createShip(ship);
+    public ResponseEntity createShip(@RequestBody Ship ship) {
+        try {
+            return ResponseEntity.ok(shipService.createShip(ship));
+        } catch (IdProvidedWhileCreationException e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
     }
 
-//    @PutMapping
-//    public Ship createShip(@RequestBody Ship ship) {
-//        return shipService.createShip(ship);
-//    }
 
     @PutMapping(path = "{id}")
-    public ResponseEntity<Ship> updateShipById(@PathVariable Integer id, @RequestBody Ship ship) {
-        Ship existingShip = shipService.getShipById(id);
-        if (existingShip != null) {
-            ResponseEntity.ok(shipService.updateShipById(id, ship));
-            return ResponseEntity.ok(ship);
-        } else {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity updateShipById(@PathVariable Integer id, @RequestBody Ship ship) {
+        try {
+            return ResponseEntity.ok(shipService.updateShipById(id, ship));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(404).body(
+                    "Can't update saw with id " + id + ". Such ship doesn't exist"
+            );
+        } catch (IdProvidedWhileUpdatingException e) {
+            return ResponseEntity.status(400).body(e.getMessage());
         }
+
     }
 
     @DeleteMapping(path = "{id}")
-    public ResponseEntity<Object> deleteShipById(@PathVariable Integer id){
-        Ship existingShip = shipService.getShipById(id);
-        if (existingShip != null){
+    public ResponseEntity<Object> deleteShipById(@PathVariable Integer id) {
+        try {
+            Ship existingSaw = shipService.getShipById(id);
             return ResponseEntity.ok(shipService.deleteShipById(id));
-        } else {
-            return ResponseEntity.notFound().build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(404).body(
+                    "Can't delete saw with id " + id + ". Such saw doesn't exist"
+            );
         }
+
+
     }
-
-
 }
 
 
